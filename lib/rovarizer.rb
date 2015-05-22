f2edc4b6e8ac4954f27a234724dd2b5f5c749467
itgsod-Rikard-Lundberg <rikard.lundberg@elev.it-gymnasiet.se>
2015-05-22 15:09:38 +0200

    färdig fungerande kod! :D

--- lib/rovarizer.rb
+++ lib/rovarizer.rb
@@ -1,8 +1,12 @@
-def rovarizer(cleartext:)
+def rovarize(cleartext:)
   consonant = ["b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N","p","P","q","Q","r","R","s","S","t","T","v","V","w","W","x","X","z","Z"]
   rovarized_cleartext = ""
   cleartext.each_char do |letter|
-    if 
-
+    if consonant.include? letter
+      rovarized_cleartext = rovarized_cleartext + (letter + "o" + letter)
+    else
+    rovarized_cleartext = rovarized_cleartext + letter
+    end
   end
+  return rovarized_cleartext
 end
