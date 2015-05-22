b40331d29f225fb8df279bd9fbc6ed633b0f84f2
itgsod-Rikard-Lundberg <rikard.lundberg@elev.it-gymnasiet.se>
2015-05-22 14:54:48 +0200

    engelska istället för svenska

--- lib/rovarizer.rb
+++ lib/rovarizer.rb
@@ -1,6 +1,8 @@
 def rovarizer(cleartext:)
-  konsonant = ["b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N","p","P","q","Q","r","R","s","S","t","T","v","V","w","W","x","X","z","Z"]
-  mening = cleartext.split("")
-  mening = mening.join("")
-  return mening
+  consonant = ["b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N","p","P","q","Q","r","R","s","S","t","T","v","V","w","W","x","X","z","Z"]
+  rovarized_cleartext = ""
+  cleartext.each_char do |letter|
+    if 
+
+  end
 end
