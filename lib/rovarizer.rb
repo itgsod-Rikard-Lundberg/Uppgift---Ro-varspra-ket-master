e2ab6ed8119801a7689d95275d4404abf0b52093
itgsod-Rikard-Lundberg <rikard.lundberg@elev.it-gymnasiet.se>
2015-05-22 16:20:21 +0200

    färdig derovarizer :DDDD

--- lib/rovarizer.rb
+++ lib/rovarizer.rb
@@ -10,3 +10,21 @@ def rovarize(cleartext:)
   end
   return rovarized_cleartext
 end
+def derovarize(cipher:)
+  consonant = ["b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N","p","P","q","Q","r","R","s","S","t","T","v","V","w","W","x","X","z","Z"]
+  cipher = cipher.split("")
+  i = 0
+  derovarized_cipher = ""
+  until i == cipher.length
+    if consonant.include? cipher[i]
+      derovarized_cipher += cipher[i]
+      i += 3
+  else
+    derovarized_cipher += cipher[i]
+    i += 1
+  end
+  end
+  p derovarized_cipher
+end
+
+
